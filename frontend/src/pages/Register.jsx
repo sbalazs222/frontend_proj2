296b5e271aaf12c552d3aead63d6942007d5ad9c
Mihalics Miron <mihalicsmiron2458@PETOFI-ASZOD.LAN>
2025-12-03 13:20:42 +0100

    Register fetch

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,43 @@
 function Register(){
+    const navigate = useNavigate();
+
+    const [formData,setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+        address: "",
+        phone_number: "",
+    });
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        const res = await fetch("http://localhost:3000/register", {
+            method:"POST",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        if (res.ok) {
+            alert("Sikeres regiszráció!")
+            navigate("/")
+        }
+        else{
+            console.log(data)
+        }
+    }
+
+    const handleChange = (e) =>{
+        setFormData({
+            ...formData,
+            [e.target.name]:e.target.value
+        })
+    }
     return(
         <>
         <h1>Regisztráció</h1>
 
-        <form>
-            <input type="text" placeholder="Name" required></input>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" onChange={handleChange} required></input>
             <input type="email" placeholder="Email"required></input>
             <input type="text" placeholder="Address" required></input>
             <input type="text" placeholder="+36 70 123 4567"></input>
