f5ff6f61828dbfc0b3ca645558cb045d5598d15c
sbalazs222 <sbalazs222@gmail.com>
2025-12-04 11:44:21 +0100

    added rate limiter

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 import express from 'express'
 import {colorLog, errorLog} from 'psgutil'
+import rateLimit from 'express-rate-limit'
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests from this IP, please try again later."
+})
 
 const app = express()
 app.use(express.json())
 app.use(colorLog)
+app.use(limiter)
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
